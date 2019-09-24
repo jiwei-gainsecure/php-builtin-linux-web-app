c10306f94ad4c6ef1d73fbf64e2cd7b2037f5c2e
Tan Ji Wei <jiwei.tan@gainsecure.net>
2019-09-24 11:11:07 +0800

    add AMS video

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -44,7 +44,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="container">
     <h1 class="text-center v-center" style="padding-bottom:20px">Get started with App Service on Linux</h1>
 </div>
-<div class="container">
+<<div class="container">
     <div class="ams-video-wrapper">
         <video width="100%" height="100%" class="azuremediaplayer amp-default-skin amp-big-play-centered" controls poster="//azurecomcdn.azureedge.net/mediahandler/files/videofiles/thumbnails/web-app-for-containers-overview/Azure_WebAppForContainers_thumb_1440x810.jpg" data-setup="{&quot;language&quot;:&quot;en&quot;,&quot;plugins&quot;:{&quot;analytics&quot;:{&quot;videoId&quot;:&quot;web-app-for-containers-overview&quot;,&quot;autoplay&quot;:false,&quot;name&quot;:&quot;Web App for Containers overview&quot;,&quot;eventsToTrack&quot;:[&quot;playTime&quot;,&quot;start&quot;,&quot;end&quot;,&quot;play&quot;,&quot;pause&quot;,&quot;error&quot;,&quot;seek&quot;,&quot;buffering&quot;,&quot;percentsPlayed&quot;]}}}">
             <source src="https://onyxmediacdn-onyxmedia.streaming.mediaservices.windows.net/a9b1b128-d5d3-45a1-a1b8-6c39aa4e8bf2/web-app-for-containers-overview.ism/manifest" type="application/vnd.ms-sstr+xml" />
@@ -58,6 +58,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     .ams-video-wrapper { width: 100%; padding-bottom: 56.25%; position: relative; background: #000 }
     .ams-video-wrapper>.azuremediaplayer { position: absolute; top: 0; bottom: 0; left: 0; right: 0 }
 </style>
+
 
 <div class="container">
         <div class="row flex text-center">
